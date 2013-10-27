e3aed03e8e22212a62c4cfdadeea46e106362490
Nejuf <matthew.b.blankenship@gmail.com>
2013-10-27 15:12:12 -0700

    Implements Task detail page

---
+++ app/assets/javascripts/views/task_detail_view.js
@@ -0,0 +1,13 @@
+TD.Views.TaskDetailView = Backbone.View.extend({
+	render: function(){
+		var that = this;
+
+		var renderedContent = JST["tasks/detail"]({
+			task: that.model
+		});
+
+		that.$el.html(renderedContent);
+		return that;
+	}
+	
+});
